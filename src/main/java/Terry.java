1021c227b55f4f78dfa97cab6db7cbc0b8dc3192
terryzhang1015 <terryzhang1015@outlook.com>
2025-01-19 18:58:15 +0800

    Initialize Chatbot Terry
    rename all 'Duke' related symbols to 'Terry'
    implement initial skeletal version chatbot

---
+++ src/main/java/Terry.java
@@ -0,0 +1,12 @@
+public class Terry {
+    public static void main(String[] args) {
+        String splitLine = "____________________________________________________________";
+        String greeting = "Hello! I'm Terry!\nWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(splitLine);
+        System.out.println(greeting);
+        System.out.println(splitLine);
+        System.out.println(bye);
+        System.out.println(splitLine);
+    }
+}
