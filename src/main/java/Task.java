f968c0e16359a94066b741774935f80e75866d90
terryzhang1015 <terryzhang1015@outlook.com>
2025-01-26 17:38:00 +0800

    Task.java: Create Task class replacing cmd

---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task() {
+        description = "";
+        isDone = false;
+    }
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
