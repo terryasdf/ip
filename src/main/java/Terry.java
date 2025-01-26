683e9ae6997d7ef6efcef46e439d7f133f1ef763
terryzhang1015 <terryzhang1015@outlook.com>
2025-01-26 17:02:10 +0800

    Create functions for print greeting and exit messages.

--- src/main/java/Terry.java
+++ src/main/java/Terry.java
@@ -1,12 +1,22 @@
 public class Terry {
-    public static void main(String[] args) {
-        String splitLine = "____________________________________________________________";
+
+    private final static String splitLine = "____________________________________________________________";
+
+    private static void printGreetingMsg() {
         String greeting = "Hello! I'm Terry!\nWhat can I do for you?";
-        String bye = "Bye. Hope to see you again soon!";
         System.out.println(splitLine);
         System.out.println(greeting);
         System.out.println(splitLine);
+    }
+
+    private static void printExitMsg() {
+        String bye = "Bye. Hope to see you again soon!";
         System.out.println(bye);
         System.out.println(splitLine);
+    }
+
+    public static void main(String[] args) {
+        printGreetingMsg();
+        printExitMsg();
     }
 }
