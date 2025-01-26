dd035ced8ef8dd40268cbff6c031741ca7458ab0
terryzhang1015 <terryzhang1015@outlook.com>
2025-01-26 18:18:46 +0800

    Terry.java: Implement feature: mark/unmark tasks
    change datatype of cmdList to Task
    rename functions
    add function: getFormattedTaskInfo for printing tasks
    modify list command to show isDone status
    add functions: markTask, unmarkTask

--- src/main/java/Terry.java
+++ src/main/java/Terry.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Terry {
 
     private final static String splitLine = "____________________________________________________________";
-    private final static String[] cmdList = new String[100];
-    private static int cmdCount = 0;
+    private final static Task[] tasks = new Task[100];
+    private static int taskCount = 0;
 
     private static void printGreetingMsg() {
         String greeting = "Hello! I'm Terry!\nWhat can I do for you?";
@@ -20,17 +20,36 @@ public class Terry {
         System.out.println(splitLine);
     }
 
-    private static void printCmdList() {
-        for (int i = 0; i < cmdCount; ++i) {
-            System.out.println(Integer.toString(i+1) + ". " + cmdList[i]);
+    private static String getFormattedTaskInfo(int id) {
+        if (id < 0 || id >= taskCount) return "";
+        return "[" + (tasks[id].isDone() ? 'X' : ' ') + "] " + tasks[id].getDescription();
+    }
+
+    private static void printTasks() {
+        for (int i = 0; i < taskCount; ++i) {
+            System.out.println(Integer.toString(i+1) + "." + getFormattedTaskInfo(i));
         }
     }
 
-    private static void addToCmdList(String cmd) {
-        if (cmdCount == 100) return;
+    private static void addTask(String description) {
+        if (taskCount == 100) return;
 
-        cmdList[cmdCount++] = cmd;
-        System.out.println("added: " + cmd);
+        tasks[taskCount++] = new Task(description);
+        System.out.println("added: " + description);
+    }
+
+    private static void markTask(int id) {
+        if (id < 0 || id >= taskCount) return;
+        tasks[id].setDone(true);
+        System.out.println("Task marked as done. Nice job.");
+        System.out.println("\t" + getFormattedTaskInfo(id));
+    }
+
+    private static void unmarkTask(int id) {
+        if (id < 0 || id >= taskCount) return;
+        tasks[id].setDone(false);
+        System.out.println("Task marked as not done.");
+        System.out.println("\t" + getFormattedTaskInfo(id));
     }
 
     private static void readCmd() {
@@ -38,20 +57,28 @@ public class Terry {
 
         while (true) {
             String cmd = in.nextLine();
+            String[] cmdArgs = cmd.split(" ");
+            if (cmdArgs.length == 0) continue;
 
+            System.out.println(splitLine);
             if (cmd.equals("bye")) {
                 printExitMsg();
                 in.close();
                 return;
             } else if (cmd.equals("list")) {
-                System.out.println(splitLine);
-                printCmdList();
-                System.out.println(splitLine);
+                printTasks();
+            } else if (cmdArgs[0].equals("mark")) {
+                if (cmdArgs.length < 2) continue;
+                int id = Integer.parseInt(cmdArgs[1]);
+                markTask(id);
+            } else if (cmdArgs[0].equals("unmark")) {
+                if (cmdArgs.length < 2) continue;
+                int id = Integer.parseInt(cmdArgs[1]);
+                unmarkTask(id);
             } else {
-                System.out.println(splitLine);
-                addToCmdList(cmd);
-                System.out.println(splitLine);
+                addTask(cmd);
             }
+            System.out.println(splitLine);
         }
     }
 
